183ae1cdae67bbcd65099ec8af851149c93616ff
Ahmed Hammad <ahm507@gmail.com>
2017-07-30 13:29:39 +0200

    Enhance schema sql code.

--- src/test/resources/schema.sql
+++ src/test/resources/schema.sql
@@ -1,22 +1,12 @@
 
-CREATE SCHEMA IF NOT EXISTS `pf_test` DEFAULT CHARACTER SET utf8 ;
+Drop database IF EXISTS pf_test;
+CREATE SCHEMA `pf_test` DEFAULT CHARACTER SET utf8 ;
 
-DROP table account;
+USE `pf_test`;
 
-CREATE TABLE  `account` (
-  `id` char(40) NOT NULL DEFAULT '',
-  `user_id` char(40) NOT NULL DEFAULT '',
-  `text` char(100) NOT NULL DEFAULT '',
-  `parent` char(40) NOT NULL DEFAULT '',
-  `description` char(225) DEFAULT '',
-  `type` char(20) NOT NULL DEFAULT '',
-  `currency` char(5) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+--  ===============================
 
-
-DROP table transaction;
-
+DROP table IF EXISTS `transaction`;
 CREATE TABLE  `transaction` (
   `id` char(40) NOT NULL DEFAULT '',
   `user_id` char(40) NOT NULL DEFAULT '',
@@ -26,10 +16,27 @@ CREATE TABLE  `transaction` (
   `deposit_id` char(40) NOT NULL DEFAULT '',
   `amount` double NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
 
 
-DROP table user;
+--  ===============================
+
+DROP table IF EXISTS account;
+
+CREATE TABLE `account` (
+  `id` char(40) NOT NULL DEFAULT '',
+  `user_id` char(40) NOT NULL DEFAULT '',
+  `text` char(100) NOT NULL DEFAULT '',
+  `parent` char(40) NOT NULL DEFAULT '',
+  `description` char(225) DEFAULT '',
+  `type` char(20) NOT NULL DEFAULT '',
+  `currency` char(5) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB;
+
+--  ===============================
+
+DROP table IF EXISTS user;
 
 CREATE TABLE  `user` (
   `id` char(40) NOT NULL DEFAULT '',
@@ -42,11 +49,29 @@ CREATE TABLE  `user` (
   `usd_rate` double DEFAULT 9.0,
   `sar_rate` double DEFAULT 2.5,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB;
+
+-- ALTER TABLE `user` ADD INDEX (`email`);
 
 
 
+-- ALTER TABLE `account` ADD INDEX (`user_id`);
+-- ALTER TABLE `account` ADD INDEX (`parent`);
+
+-- =========================================================
 
 
 
+--
+--
+-- ALTER TABLE `transaction` ADD INDEX (`user_id`);
+-- ALTER TABLE `transaction` ADD INDEX (`date`);
+-- ALTER TABLE `transaction` ADD INDEX (`withdraw_id`);
+-- ALTER TABLE `transaction` ADD INDEX (`deposit_id`);
+
+
+-- Add Rleationships
+
+-- ALTER TABLE `account` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
+-- ALTER TABLE `transaction` ADD FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 
